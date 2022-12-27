27ad7ff052885045087bfd7c1f9950d02e49c7ea
Lukasz <lukasz.finster@gmail.com>
2022-12-27 13:05:25 +0100

    corrections for windows and linux

---
+++ MySensorsLF.pri
@@ -0,0 +1,16 @@
+INCLUDEPATH += $$PWD/src
+
+SOURCES += \
+        $$PWD/src/MySensors/MyHelperFunctions.cpp \
+        $$PWD/src/MySensors/MyMessage.cpp \
+        $$PWD/src/mysensorslfgateway.cpp \
+        $$PWD/transport.cpp
+
+HEADERS += \
+    $$PWD/src/MySensors/MyHelperFunctions.h \
+    $$PWD/src/MySensors/MyIndication.h \
+    $$PWD/src/MySensors/MyMessage.h \
+    $$PWD/src/MySensors/Version.h \
+    $$PWD/src/defines.h \
+    $$PWD/src/mysensorslfgateway.h \
+    $$PWD/transport.h
--- src/MySensors/MyMessage.cpp
+++ src/MySensors/MyMessage.cpp
@@ -26,6 +26,42 @@
 
 #include "defines.h"
 
+#if M_OS == M_OS_LINUX || M_OS == M_OS_WINDOWS
+#include <cmath>
+char* dtostrf (double val, signed char width, unsigned char prec, char *sout)
+{
+    (void)width;
+    uint32_t iPart = (uint32_t)val;
+    sprintf(sout, "%d", iPart);
+
+    if (prec > 0)
+    {
+        uint8_t pos = strlen(sout);
+        sout[pos++] = '.';
+        uint32_t dPart = (uint32_t)((val - (double)iPart) * pow(10, prec));
+
+        for (uint8_t i = (prec - 1); i > 0; i--)
+        {
+            size_t pow10 = pow(10, i);
+            if (dPart < pow10)
+            {
+                sout[pos++] = '0';
+            }
+            else
+            {
+                sout[pos++] = '0' + dPart / pow10;
+                dPart = dPart % pow10;
+            }
+        }
+
+        sout[pos++] = '0' + dPart;
+        sout[pos] = '\0';
+    }
+
+    return sout;
+}
+#endif
+
 MyMessage::MyMessage(void)
 {
     this->clear();
